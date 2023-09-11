5ab31dfeef6678aeb1643803a6e600d83742fc0c
vashu992 <ashutoshpratap992@gmail.com>
2023-09-11 13:05:07 +0530

    Fever

---
+++ Fever/main.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main(){
+	var t int
+	fmt.Scan(&t)
+	
+	for i := 0; i <t; i++ {
+	  var x int
+	  fmt.Scan(&x)
+	  
+	  if x > 98 {
+	    fmt.Println("YES")
+	  }else {
+	    fmt.Println("NO")
+	  }
+	}
+}
